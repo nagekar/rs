debee5db28e3991f6601f5bba71140c721dc1dec
nagekar <srikarunasrikarnakoti@gmail.com>
2026-05-28 23:42:24 +0530

    Create Contact.js

---
+++ Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+
+      <h2>Contact Page</h2>
+
+      <p>Email: support@studentms.com</p>
+
+      <p>Phone: +91 9876543210</p>
+
+    </div>
+  );
+}
+
+export default Contact;
