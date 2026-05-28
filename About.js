c1c618ad35c1556a86c8bf1d577342f96d1e615a
nagekar <srikarunasrikarnakoti@gmail.com>
2026-05-28 23:41:52 +0530

    Create About.js

---
+++ About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+
+      <h2>About Page</h2>
+
+      <p>
+        This is a simple React Student Management System project.
+      </p>
+
+    </div>
+  );
+}
+
+export default About;
